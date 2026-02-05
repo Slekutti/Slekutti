e42aa77a91f96d1ebc401d648fa024d4ee759c27
Slekutti <155874433+Slekutti@users.noreply.github.com>
2026-02-05 23:50:00 +0800

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,9 +15,9 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  title: "Slekutti",
+  url: "https://toyhou.se/~world/161434.slekutti",
+  description: `This is the official Slekutti Masterlist website.`
 }
 
 /* ==================================================================== */
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1PEG0nY2alEZ1gniyZOrYy1GROI0cZ-Cyd8PRz-VyuMQ",
 
   pages: {
     masterlist:    "masterlist",
@@ -44,9 +44,9 @@ charadex.sheet = {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
-    itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
+    rarity: ['All', 'Standard', 'Mutation', 'Mythical', 'Modified'],
+    species: ['All', 'Standard', 'Mutations', 'Mythical'],
+    itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Cosmetics', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
 
   }
